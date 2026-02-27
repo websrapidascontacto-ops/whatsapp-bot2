030d1646ecdca729cf7997be7dd6a1c6aee16a51
TuNombre <tuemail@example.com>
2026-02-27 08:30:46 -0500

    Actualizacion automatica vie 27/02/2026  8:30:46.75

--- server.js
+++ server.js
@@ -1000,6 +1000,6 @@ module.exports = {
 };
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Servidor corriendo en puerto", PORT);
 });
